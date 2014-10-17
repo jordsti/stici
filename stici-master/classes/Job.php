b740f0eb7429e8e1dec9999dc292264971992f1d
= <jord52@gmail.com>
2014-10-17 09:12:09 -0400

    revert

--- stici-master/classes/Job.php
+++ stici-master/classes/Job.php
@@ -2,12 +2,17 @@
 
 class Job
 {
-	private $id;
-	private $name;
-	private $status;
-	private $buildNumber;
-	private $remoteGit;
+	public $id;
+	public $name;
+	public $status;
+	public $buildNumber;
+	public $remoteGit;
 	public $stamp;
+	public $flags;
+	
+	
+	public static $Win32Build = 1;
+	public static $LinuxBuild = 2;
 	
 	public static $Building = 0;
 	public static $Success = 1;
@@ -21,6 +26,24 @@ class Job
 		$this->buildNumber = $data['build_number'];
 		$this->remoteGit = $data['remote_git'];
 		$this->stamp = 0;
+		$this->flags = $data['flags'];
+	}
+	
+	public function addFlags($flag)
+	{
+		$this->flags = $this->flags | $flag;
+	}
+	
+	public function removeFlags($flag)
+	{
+		$this->flags = $this->flags ^ $flag;
+	}
+	
+	public function testFlags($flag)
+	{
+		$if = $this->flags & $flag;
+		return ($if == $flag);
+		
 	}
 	
 	public function getStatusText()

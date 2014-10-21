d9988ed55d22fd6c0128a3fe3abf32ab1132640c
= <jord52@gmail.com>
2014-10-20 22:53:46 -0400

    little settings change

--- stici-master/classes/Settings.php
+++ stici-master/classes/Settings.php
@@ -31,6 +31,7 @@ class Settings
 		$this->settings['username_max_char'] = 32;
 		$this->settings['username_min_char'] = 5;
 		$this->settings['password_min_char'] = 5;
+		$this->settings['']
 	}
 	
 	public function save()
